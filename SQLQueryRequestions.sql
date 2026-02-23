fdd4cf16ce59cf2dff0251110d8f7033476b120e
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:45:31 +0200

    Create SQLQueryRequestions.sql

---
+++ SQLQueryRequestions.sql
@@ -0,0 +1,60 @@
+﻿--1--
+SELECT [Name],[Financing],[Id]
+FROM Departments;
+--2--
+SELECT
+	[Name] AS 'Group Name',
+	[Rating] AS 'Group Rating'
+FROM Groups;
+--4--
+SELECT 
+    'The dean of faculty ' + [Name] + ' is ' + [Dean] + '.' AS 'Info'
+FROM Faculties;
+--5--
+SELECT [Surname]
+FROM Teachers
+WHERE [IsProfessor] = 1 AND [Salary] > 1050;
+--6--
+SELECT [Name]
+FROM Departments
+WHERE [Financing] < 11000 OR [Financing] > 25000;
+--7--
+SELECT [Name] 
+FROM Faculties
+WHERE NAME <> N'Computer Science'
+--8--
+SELECT [Surname], [Position]
+FROM Teachers
+WHERE [IsProfessor] = 0;
+--9--
+SELECT [Surname], [Position], [Salary], [Premium]
+FROM Teachers
+WHERE [IsAssistant] = 1 
+AND [Premium] BETWEEN 160 AND 550;
+--10--
+SELECT [Surname], [Salary]
+FROM Teachers
+WHERE [IsAssistant] = 1;
+--11--
+SELECT [Surname], [Position]
+FROM Teachers
+WHERE [EmploymentDate] < '2000-01-01';
+--13--
+SELECT [Surname]
+FROM Teachers
+WHERE [IsAssistant] = 1 
+AND ([Salary] + [Premium]) <= 1200;
+--14--
+SELECT [Name]
+FROM Groups
+WHERE [Year] = 5 
+AND [Rating] BETWEEN 2 AND 4;
+--15--
+SELECT [Surname]
+FROM Teachers
+WHERE [IsAssistant] = 1 
+AND ([Salary] < 550 OR [Premium] < 200);
+
+
+
+
