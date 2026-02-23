13113b20148ccc8be003c905fb7119687eae6744
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:46:14 +0200

    Create SQLQueryGroups.sql

---
+++ SQLQueryGroups.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Groups(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(10) NOT NULL UNIQUE
+        CHECK(Name <> ''),
+    [Rating] INT NOT NULL
+        CHECK(Rating >= 0 AND Rating <= 5),
+    [Year] INT NOT NULL
+        CHECK(Year >= 1 AND Year <= 5)
+)
