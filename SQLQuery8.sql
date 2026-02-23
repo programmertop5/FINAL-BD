2c18c3b898b5246153732b0e824c486b559d3c0e
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:51:33 +0200

    Create SQLQuery8.sql

---
+++ SQLQuery8.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Teachers (
+    Id INT NOT NULL  PRIMARY KEY IDENTITY,
+    Name NVARCHAR(MAX) NOT NULL, 
+    CHECK (Name <> ''),
+    Surname NVARCHAR(MAX) NOT NULL, 
+    CHECK (Surname <> ''),
+    Salary MONEY NOT NULL, 
+    CHECK (Salary > 0)
+);
