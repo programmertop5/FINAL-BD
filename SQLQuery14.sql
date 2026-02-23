48896baff4359a568478b5ae664522d3094ff08b
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:48:54 +0200

    Create SQLQuery14.sql

---
+++ SQLQuery14.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE GroupsLectures (
+    Id INT NOT NULL PRIMARY KEY IDENTITY,
+    GroupId INT NOT NULL,
+    LectureId INT NOT NULL,
+    FOREIGN KEY (GroupId) REFERENCES Groups(Id),
+    FOREIGN KEY (LectureId) REFERENCES Lectures(Id)
+);
