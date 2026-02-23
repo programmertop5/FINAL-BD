fcdb79b14a9a0d3317f6907b8ae1beb8ad46d574
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:49:12 +0200

    Create SQLQuery13.sql

---
+++ SQLQuery13.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE Lectures (
+    Id INT NOT NULL PRIMARY KEY IDENTITY,
+    DayOfWeek INT NOT NULL,
+    CHECK (DayOfWeek BETWEEN 1 AND 7),
+    LectureRoom NVARCHAR(MAX) NOT NULL, 
+    CHECK (LectureRoom <> ''),
+    SubjectId INT NOT NULL,
+    TeacherId INT NOT NULL,
+    FOREIGN KEY (SubjectId) REFERENCES Subjects(Id),
+    FOREIGN KEY (TeacherId) REFERENCES Teachers(Id)
+);
