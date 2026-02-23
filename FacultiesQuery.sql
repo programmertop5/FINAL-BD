41c8992537f4e6a6844da697a2f56d5f3e8557d7
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:42:32 +0200

    Update FacultiesQuery.sql

--- FacultiesQuery.sql
+++ FacultiesQuery.sql
@@ -1,4 +1,13 @@
-CREATE TABLE Faculties(
+﻿CREATE TABLE Faculties(
 	[Id]  INT  NOT NULL PRIMARY KEY IDENTITY,
 	[Name]  NVARCHAR(100)  NOT NULL UNIQUE
 )
+
+INSERT INTO Faculties ([Name]) VALUES
+(N'Факультет комп''ютерних наук'),
+(N'Факультет менеджменту'),
+(N'Факультет медицини'),
+(N'Факультет іноземних мов'),
+(N'Факультет архітектури'),
+(N'Факультет математики та фізики'),
+(N'Факультет біології');
