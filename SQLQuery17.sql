30926465b144a6f487411bdb9c6edd0d5bcb4756
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:51:14 +0200

    Create SQLQuery17.sql

---
+++ SQLQuery17.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Department (
+    Id INT NOT NULL PRIMARY KEY IDENTITY,
+    Financing MONEY NOT NULL DEFAULT 0, 
+    CHECK(Financing >= 0),
+    Name NVARCHAR(100) NOT NULL UNIQUE, 
+    CHECK(Name <> ''),
+    FacultyId INT NOT NULL,
+    FOREIGN KEY (FacultyId) REFERENCES Faculties(Id)
+);
