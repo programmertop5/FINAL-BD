80e6484413386b0d2a82b8e8055bd9e72f1d9970
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:36:18 +0200

    Create TeachersQuery.sql

---
+++ TeachersQuery.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Teachers(
+	[Id]  INT NOT NULL PRIMARY KEY IDENTITY,
+	[EmploymentDate]  DATE  NOT NULL
+	CHECK (EmploymentDate >= '1990-01-01'),
+	[Name]  NVARCHAR(MAX)  NOT NULL,
+	[Premium]  MONEY  NOT NULL DEFAULT 0
+	CHECK (Premium >= 0),
+	[Salary]  MONEY  NOT NULL
+	CHECK(Salary <= 0),
+	[Surname]  NVARCHAR(MAX)  NOT NULL
+)
+
