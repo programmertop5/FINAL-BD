6825d790778cae1191ea3e67283449c4e519ead9
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:46:54 +0200

    Create SQLQueryFaculties2.sql

---
+++ SQLQueryFaculties2.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE Faculties(
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Dean] NVARCHAR(MAX) NOT NULL
+        CHECK(Dean <> ''),
+    [Name] NVARCHAR(100) NOT NULL UNIQUE
+        CHECK(Name <> '')
+)
