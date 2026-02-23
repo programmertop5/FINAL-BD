fd7d825e87f776646dbe07908602f50c0936cbb8
programmertop5 <kardrkarina@gmail.com>
2026-02-23 15:48:29 +0200

    Create SQLQuery15.sql

---
+++ SQLQuery15.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Groups (
+    Id INT NOT NULL PRIMARY KEY IDENTITY,
+    Name NVARCHAR(10) NOT NULL UNIQUE, 
+    CHECK (Name <> ''),
+    Year INT NOT NULL,
+    CHECK (Year BETWEEN 1 AND 5),
+    DepartmentId INT NOT NULL,
+    FOREIGN KEY (DepartmentId) REFERENCES Departments(Id)
+);
